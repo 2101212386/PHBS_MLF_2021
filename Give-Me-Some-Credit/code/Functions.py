ca7b483b717ad1d8c2c392bcdd3cc227d4f3dba5
Zhang Bochi <125948816+2101212386@users.noreply.github.com>
2023-03-21 21:22:39 +0800

    Add files via upload

---
+++ Give-Me-Some-Credit/code/Functions.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 21 13:42:48 2023
+
+@author: zhangbochi
+"""
+
+import numpy as np
+import pandas as pd
+
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+
+# To check recent matplotlib compatibility
+import matplotlib
+from distutils.version import LooseVersion
+
+
+# Remove outlier
+def RemoveOutlier(Data):
+    Q1, Q3 = np.percentile(Data, 25, axis=0), np.percentile(Data, 75, axis=0)
+    IQR = Q3 - Q1
+    
+    Cond = (Data >= Q1 - 1.5 * IQR) & (Data <= Q3 + 1.5 * IQR)
+    
+    Mean = np.mean(Data[Cond].dropna().copy(), axis=0)
+    
+#     _Data_ = Data[Cond].fillna(Mean).copy()
+    
+    _Data_ = Data[Cond].fillna(Data.median()).copy()
+    return _Data_
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.8, 
+                    color=colors[idx],
+                    marker=markers[idx], 
+                    label=cl, 
+                    edgecolor='black')
+
+    # highlight test examples
+    if test_idx:
+        # plot all examples
+        X_test, y_test = X[test_idx, :], y[test_idx]
+
+        
+        if LooseVersion(matplotlib.__version__) < LooseVersion('0.3.4'):
+            plt.scatter(X_test[:, 0],
+                        X_test[:, 1],
+                        c='',
+                        edgecolor='black',
+                        alpha=1.0,
+                        linewidth=1,
+                        marker='o',
+                        s=100, 
+                        label='test set')
+        else:
+            plt.scatter(X_test[:, 0],
+                        X_test[:, 1],
+                        c='none',
+                        edgecolor='black',
+                        alpha=1.0,
+                        linewidth=1,
+                        marker='o',
+                        s=100, 
+                        label='test set')        
